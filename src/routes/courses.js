48fbbcaaa366dd797b0be9dafd43e4c37944fc21
marant15 <pensu15@gmail.com>
2019-07-14 18:18:30 -0400

    [BE]new endpoints with dates

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -116,4 +116,27 @@ router.get('/exc/:id', async(req, res) => {
     const result = await pool.query('SELECT * FROM excepcion WHERE excepcionID=?',[req.params.id]);
     res.json(result);
 })
+
+router.get('/registros/:fecha1/:fecha2/:codigo', async(req, res) => {
+    const result = await pool.query('select fechaRegistro, profesor.nombre as nombre, apellidoM, apellidoP, fechaInicio, fechaFin, horaInicio, horaFin, '+
+    'grupo.nombre as grupo, materia.nombre as materia, fechaContratacion, codigo from registro inner join '+
+    'asignacion on registro.asignacionID = asignacion.asignacionID inner join '+
+    'grupo on grupo.grupoID = asignacion.grupoID inner join '+
+    'materia on materia.materiaID = asignacion.materiaID inner join '+
+    'profesor on profesor.profesorID = asignacion.profesorID '+
+    'where (DATE(fechaRegistro) BETWEEN ? AND ?) AND codigo = ?',[req.params.fecha1,req.params.fecha2,req.params.codigo]);
+    res.json(result);
+})
+
+router.get('/excs/:fecha1/:fecha2/:codigo', async(req, res) => {
+    const result = await pool.query('select fechaExcepcion, profesor.nombre as nombre, apellidoM, apellidoP, fechaInicio, fechaFin, horaInicio, horaFin, '+
+    'tipo, grupo.nombre as grupo, materia.nombre as materia, fechaContratacion, codigo from excepcion inner join '+
+    'asignacion on excepcion.asignacionID = asignacion.asignacionID inner join '+
+    'grupo on grupo.grupoID = asignacion.grupoID inner join '+
+    'materia on materia.materiaID = asignacion.materiaID inner join '+
+    'profesor on profesor.profesorID = asignacion.profesorID '+
+    'where (DATE(fechaExcepcion) BETWEEN ? AND ?) AND codigo = ?',[req.params.fecha1,req.params.fecha2,req.params.codigo]);
+    res.json(result);
+})
+
 module.exports = router;
